dab9160fa7a84166de2083da934f672a7b70f9a9
Semio Zheng <prairy.long@gmail.com>
2016-05-25 11:19:31 +0800

    include new columns

--- etl/script/tfr.py
+++ etl/script/tfr.py
@@ -10,6 +10,20 @@ source = '../source/gapdata008 v6.xlsx'
 out_dir = '../../'
 
 
+def extract_datapoints(data, col, col_id):
+    """extract datapoints from source.
+
+    :parameters:
+    col: target column
+    col_id: the concept id of target column
+    """
+    dp = data[['Area', 'Year', col]].copy()
+    dp.columns = ['area', 'year', col_id]
+    dp['area'] = dp['area'].map(to_concept_id)
+
+    return dp
+
+
 if __name__ == '__main__':
     data001 = pd.read_excel(source, sheetname='Data & metadata')
 
@@ -24,20 +38,33 @@ if __name__ == '__main__':
     ent.to_csv(path, index=False)
 
     # datapoints
-    data001_dp_1 = data001[['Area', 'Year', 'Total Fertility Rate (TFR), also called Children per Woman']].copy()
-    data001_dp_2 = data001[['Area', 'Year', 'TFR interpolated']].copy()
+    dps = {
+        'Total Fertility Rate (TFR), also called Children per Woman': 'total_fertility_rate',
+        'TFR interpolated': 'total_fertility_rate_interpolated',
+        'Crude Birth Rate (CBR)': 'crude_birth_rate',
+        'Princeton If index': to_concept_id('Princeton If index')
+    }
 
-    data001_dp_1.columns = ['area', 'year', 'total_fertility_rate']
-    data001_dp_2.columns = ['area', 'year', 'total_fertility_rate_interpolated']
+    for col, col_id in dps.items():
+        dp = extract_datapoints(data001, col, col_id)
+        path = os.path.join(out_dir, 'ddf--datapoints--{}--by--area--year.csv'.format(col_id))
 
-    data001_dp_1['area'] = data001_dp_1['area'].map(to_concept_id)
-    data001_dp_2['area'] = data001_dp_2['area'].map(to_concept_id)
+        dp.dropna().sort_values(by=['area', 'year']).to_csv(path, index=False)
 
-    path1 = os.path.join(out_dir, 'ddf--datapoints--total_fertility_rate--by--area--year.csv')
-    path2 = os.path.join(out_dir, 'ddf--datapoints--total_fertility_rate_interpolated--by--area--year.csv')
+    # data001_dp_1 = data001[['Area', 'Year', 'Total Fertility Rate (TFR), also called Children per Woman']].copy()
+    # data001_dp_2 = data001[['Area', 'Year', 'TFR interpolated']].copy()
 
-    data001_dp_1.dropna().sort_values(by=['area', 'year']).to_csv(path1, index=False)
-    data001_dp_2.dropna().sort_values(by=['area', 'year']).to_csv(path2, index=False)
+    # data001_dp_1.columns = ['area', 'year', 'total_fertility_rate']
+    # data001_dp_2.columns = ['area', 'year', 'total_fertility_rate_interpolated']
+
+    # data001_dp_1['area'] = data001_dp_1['area'].map(to_concept_id)
+    # data001_dp_2['area'] = data001_dp_2['area'].map(to_concept_id)
+
+    # path1 = os.path.join(out_dir, 'ddf--datapoints--total_fertility_rate--by--area--year.csv')
+    # path2 = os.path.join(out_dir, 'ddf--datapoints--total_fertility_rate_interpolated--by--area--year.csv')
+
+    # data001_dp_1.dropna().sort_values(by=['area', 'year']).to_csv(path1, index=False)
+    # data001_dp_2.dropna().sort_values(by=['area', 'year']).to_csv(path2, index=False)
 
     # concept
     conc = ['total_fertility_rate', 'total_fertility_rate_interpolated', 'area', 'year', 'name']
